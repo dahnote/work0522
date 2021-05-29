17f2051b41e6c1aceedc17db2f8db669d14bde3b
dahnote <dahnote1993@gmail.com>
2021-05-29 20:48:58 +0800

    整理檔案結構

---
+++ week4/js/index.js
@@ -0,0 +1,33 @@
+import { createApp } from 'https://cdnjs.cloudflare.com/ajax/libs/vue/3.0.9/vue.esm-browser.js';
+createApp({
+    data() {
+      return {
+        url: 'https://vue3-course-api.hexschool.io/',
+        loginData:{
+            username:'',
+            password:''
+        }
+      };
+    },
+    methods: {
+      login() {
+        if (this.loginData.username!=='' & this.loginData.username!==''){
+          axios.post(`${this.url}admin/signin`,this.loginData)
+            .then((res)=>{
+              if (res.data.success){
+                  alert(res.data.message);
+                  const token=res.data.token;
+                  const expired=res.data.expired;
+                  document.cookie = `mytoken=${token}; expires=${new Date(expired)}`;
+                  window.location = './html/products.html';
+              }else{
+                  alert(res.data.message);
+              }
+            })
+            .catch(err => {
+              console.log(err.response);
+            })
+        }
+      }
+    },
+  }).mount('#app');
---
+++ week4/js/products.js
@@ -0,0 +1,140 @@
+import { createApp } from 'https://cdnjs.cloudflare.com/ajax/libs/vue/3.0.9/vue.esm-browser.js';
+// let productModal = null;
+createApp({
+    data() {
+      return {
+        url: 'https://vue3-course-api.hexschool.io',
+        api_path:'hua1993',
+        page:'1',
+        loginData:{
+            username:'',
+            password:''
+        },
+        token:'',
+        productsData:[],
+        mode:'',
+        productModal:{},
+        delProductModal:{},
+        delProductName:'',
+        productId:'',
+        productInfo:{
+          imagesUrl: [],
+          is_enabled:0
+        },
+        imagesUrl:[],
+        alertMessage:''
+      };
+    },
+    mounted() {
+      this.productModal = new bootstrap.Modal(document.getElementById('productModal'), {
+        keyboard: false
+      });
+      this.delProductModal = new bootstrap.Modal(document.getElementById('delProductModal'), {
+        keyboard: false
+      });
+      this.token=document.cookie.replace(/(?:(?:^|.*;\s*)mytoken\s*\=\s*([^;]*).*$)|^.*$/, "$1");
+      axios.defaults.headers.common['Authorization'] = this.token;
+      this.getData();
+      
+    },
+    methods: {
+      getData() {
+        ///api/:api_path/admin/products?page=:page
+        axios(`${this.url}/api/${this.api_path}/admin/products/`)
+          .then((res)=>{
+            if (res.data.success){
+              this.productsData=res.data.products
+            }
+          })
+          .catch(err => {
+            console.log(err.response);
+          })
+       },
+      editModal(mode,index){
+        this.mode=mode;
+        this.alertMessage='';
+        if(mode==='create'){
+          this.productInfo={};
+          this.productInfo.imagesUrl= [];
+          this.productInfo.is_enabled=0;
+          this.productModal.show();
+        }else{
+          this.productId=this.productsData[index].id;
+          this.productInfo={...this.productsData[index]};
+          this.productModal.show();
+        }
+      },
+      delModal(id,title){
+        this.delProductName=title;
+        if (id==='close'){
+          this.delProductModal.hide();
+        }else{
+          this.productId=id;
+          this.delProductModal.show();
+        }
+      },
+      processingflow(){
+        if(this.mode==='create'){
+          this.createProducet()
+        }else if(this.mode==='edit'){
+          this.updateProducet()
+        }
+      },
+      createProducet(){
+        axios.post(`${this.url}/api/${this.api_path}/admin/product`,{ "data": this.productInfo})
+        .then((res)=>{
+          if (res.data.success){
+            alert(res.data.message);
+            this.productModal.hide();
+            this.getData();
+          }else{
+            this.alertMessage=res.data.message[0]
+          }
+          // this.data.productsData=res.data.products;      
+        })
+        .catch(err => {
+          console.log(err.response);
+        })
+      },
+      updateProducet(){
+        axios.put(`${this.url}/api/${this.api_path}/admin/product/${this.productId}`,{ "data": this.productInfo})
+        .then((res)=>{
+          if (res.data.success){
+            alert(res.data.message);
+            this.productModal.hide();
+            this.getData();
+          }else{
+            console.log(res.data.message[0]);
+          }
+          // this.data.productsData=res.data.products;      
+        })
+        .catch(err => {
+          console.log(err.response);
+        })
+      },
+      deleteProducet(){
+        axios.delete(`${this.url}/api/${this.api_path}/admin/product/${this.productId}`)
+          .then((res)=>{
+            // console.log(res);
+            alert(res.data.message);
+            if (res.data.success){
+              this.delProductModal.hide();
+              this.getData();
+            }
+          })
+          .catch(err => {
+            console.log(err.response);
+          })
+      },
+      addImage(){
+        if (this.imagesUrl!==''){
+          this.productInfo.imagesUrl.push(this.imagesUrl);
+          this.imagesUrl='';
+        }
+      },
+      delImage(index){
+        this.productInfo.imagesUrl.splice(index, 1);
+      }
+
+    },
+  }).mount('#app');
