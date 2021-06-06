dd5485ed8ffd1611d9eb1847b6f86a04dd009ade
dahnote <dahnote1993@gmail.com>
2021-06-06 13:09:06 +0800

    feat:購物車更新刪除

--- week5/js/index.js
+++ week5/js/index.js
@@ -9,18 +9,8 @@ createApp({
         api_path:'hua1993',
         page:1,
         pagination:{},
-        loginData:{
-            username:'',
-            password:''
-        },
-        productModal:'',
         productsData:[],
-        delProductModal:{},
-        delProductObj:{
-          productId:'',
-          productName:'',
-        },
-        btn:'',
+        cartData:[],
         infoId:''
       };
     },
@@ -28,7 +18,8 @@ createApp({
       infomodal,pagination
     },
     mounted() {
-      this.getData(); 
+      this.getData();
+      this.getCart();
     },
     methods: {
       getData(item) {
@@ -50,19 +41,71 @@ createApp({
           .catch(err => {
             console.log(err.response);
           })
-       },
+      },
+      getCart(){
+        axios(`${this.url}/api/${this.api_path}/cart`)
+          .then((res)=>{
+            if (res.data.success){
+              // console.log(res.data);
+              this.cartData=res.data.data.carts;
+            }
+          })
+          .catch(err => {
+            console.log(err.response);
+          })
+      },
+      addCart(id){
+        // console.log(id);
+        const postData={ product_id: id,"qty":1};
+        axios.post(`${this.url}/api/${this.api_path}/cart`,{ "data":postData })
+        .then((res)=>{
+          if (res.data.success){
+            alert(res.data.message);
+            this.getCart();
+          }else{
+            this.alertMessage=res.data.message[0];
+          }
+          // this.data.productsData=res.data.products;      
+        })
+        .catch(err => {
+          console.log(err.response);
+        })
+      },
+      updataCart(index){
+        // if (this.cartData[index])
+        const postData={ product_id: this.cartData[index].id,"qty":this.cartData[index].qty};
+        axios.put(`${this.url}/api/${this.api_path}/cart/${this.cartData[index].id}`,{ "data":postData })
+        .then((res)=>{
+          if (res.data.success){
+            alert(res.data.message);
+            this.getCart();
+          }else{
+            alert(res.data.message);
+          }
+          // this.data.productsData=res.data.products;      
+        })
+        .catch(err => {
+          console.log(err.response);
+        })
+      },
+      deleteItem(id){
+        axios.delete(`${this.url}/api/${this.api_path}/cart/${id}`)
+        .then((res)=>{
+          // console.log(res);
+          if (res.data.success){
+            alert(res.data.message);
+            this.getCart();
+          }else{
+            alert(res.data.message);
+          }
+        })
+        .catch(err => {
+          console.log(err.response);
+        })
+      },
       editModal(btn,index){
         this.infoId=this.productsData[index].id;
         this.$refs.infoComponent.open();
-      },
-      delModal(id,title){
-        if (id==='close'){
-          this.delProductModal.hide();
-        }else{
-          this.delProductObj.productId=id;
-          this.delProductObj.productName=title;
-          this.delProductModal.show();
-        }
       }
     },
   }).mount('#app');
